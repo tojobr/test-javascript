56f2e6d3b3f92a3ab584a0278b017d99d1f284a2
tojobr <tojo.bariniaina.23@gmail.com>
2022-04-21 19:09:31 +0300

    Create example1.js
    isTwin

---
+++ example1.js
@@ -0,0 +1,27 @@
+/**
+ * check if the two words are twins
+ * each character of first word exists on second word
+ * @param {string} a 
+ * @param {string} b 
+ * @returns {boolean}
+ */
+function isTwin(a, b) {
+	a = a.toLowerCase();
+	b = b.toLowerCase();
+
+	const indexes = [];
+	for (let i = 0; i < a.length; i++) {
+		const c = a.charAt(i);
+		let index = b.indexOf(c);
+		if (index >= 0) {
+			b = b.replace(c, '$');
+		}
+		indexes[i] = index;
+	}
+
+	return indexes.every(i => i >= 0);
+}
+
+console.log(check('Hello', 'World'));
+console.log(check('abc', 'bAc'));
+console.log(check('uOtlook', 'Lookout'));
